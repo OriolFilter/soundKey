0ebe2bbbd54b5cbc5f03b49f378f633bcfc00a5d
OriolFilter <filter.oriol@gmail.com>
2020-04-11 22:32:22 +0200

    v0.1

--- soundKey.py
+++ soundKey.py
@@ -10,21 +10,35 @@ soundFolder="sound/"
 
 character="ivern"
 
-spell1="q"
-spell2="w"
-spell3="e"
-spell4="r"
+# spell1="q"
+# spell2="w"
+# spell3="e"
+# spell4="r"
+#
+# summon1="d"
+# summon2="f"
 
-summon1="d"
-summon2="f"
+alternativeKeysArry=["space"]
 summonDic={"f":"flash","i":"ignite","s":"smite"}
 
-exit="esc"
+spellArry=[
+    ["q","Q"],#s1
+    ["w","W"],#s2
+    ["e","E"],#s3
+    ["r","R"]#s4
+]
+summonArry=[
+    ["d","f"],#s1
+    ["f","s"]#s2
+]
+
+exit="shift+esc"
 
 # windowsName="Discord" #test
+# windowsName="Discord"
 windowsName="League of Legends (TM) Client"
 
-
+close=False
 
 def checkActiveWindow():
     try:
@@ -46,6 +60,7 @@ def spell(S=None,D="spells/"): #S
 def summoner(S=None,D="summoners/"): #Summoner #Directory
     if checkActiveWindow() and checkSpellUp():
         link=soundFolder+D+summonDic[S]+".mp3"
+        # print(link)
         bkgPlay = threading.Thread(target=playsound, args=(link,))
         bkgPlay.start()
 
@@ -62,11 +77,31 @@ def checkSummonerUp(S=None):
         return True
     else: return False
 
-keyboard.add_hotkey(spell1, spell,args="Q")
-keyboard.add_hotkey(spell2, spell,args="W")
-keyboard.add_hotkey(spell3, spell,args="E")
-keyboard.add_hotkey(spell4, spell,args="R")
-keyboard.add_hotkey(summon1, summoner, args=("f"))
-keyboard.add_hotkey(summon2, summoner, args=("s"))
 
-keyboard.wait(exit)
+# print(spellArry[0][0])
+# x=0
+
+for key,spellL in spellArry:
+    keyboard.add_hotkey(key, spell, args=spellL)
+    for altKey in alternativeKeysArry:
+        keyboard.add_hotkey(key + "+"+altKey, spell, args=spellL)
+    # print(key)
+
+for key,summL in summonArry:
+    keyboard.add_hotkey(key, summoner, args=summL)
+    for altKey in alternativeKeysArry:
+        keyboard.add_hotkey(key + "+"+altKey, summoner, args=summL)
+    # # print(key,summL)
+
+# keyboard.add_hotkey("d", spell, args="f")
+
+
+while not close:
+    try:
+        keyboard.wait(exit)
+        close=True
+    except: pass
+
+# keyboard.add_hotkey(summon1, summoner, args=("f"))
+
+
